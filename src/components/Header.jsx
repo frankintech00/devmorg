b6935b777cb8515745f1fc2e748b18f883b9d742
Frank Kelly <frankintech00@gmail.com>
2023-02-17 05:14:58 +0000

    added margin to icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { FcHome } from 'react-icons/fc';
 const Header = () => {
 	return (
 		<div className='flex flex-wrap justify-center align-middle'>
-			<FcHome size={120} />
+			<FcHome size={120} className='mx-5' />
 			<h1 className='m-4 my-auto text-5xl text-center'>Mortgage Calculator</h1>
 		</div>
 	);
